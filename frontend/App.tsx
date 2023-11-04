b43ad5392cc317c21b537751ceca3ce3dd618847
Madhur Gupta <madhurgupta107@gmail.com>
2023-11-05 01:19:56 +0530

    Fix getLLMResponse

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-unstable-nested-components */
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
@@ -14,6 +14,7 @@ import Login from './src/screens/Login/Login';
 import useAuthStore from './src/store/AuthStore';
 import Signup from './src/screens/Signup/Signup';
 import Login2 from './src/screens/Login/Login2';
+import getPropertyFromPrompt from './src/utils/getPropertiesFromPrompt';
 
 const AppStack = createNativeStackNavigator<AppRoutes>();
 const AuthenticationStack = createNativeStackNavigator<AuthenticationRoutes>();
@@ -21,6 +22,14 @@ const AuthenticationStack = createNativeStackNavigator<AuthenticationRoutes>();
 function App(): JSX.Element {
   const isFirstLaunch = useFirstLaunch();
   const isLoggedIn = useAuthStore(state => state.isLoggedIn);
+
+  useEffect(() => {
+    (async () => {
+      await getPropertyFromPrompt(
+        'I am traveling with my dog. I want to visit lakes, parks during my journey. I can give a maximum of 2 hours for my journey.',
+      );
+    })();
+  }, []);
 
   console.log('IS LOGGED IN: ', isLoggedIn);
   if (isFirstLaunch === null || isLoggedIn === null) {
--- frontend/src/utils/getPropertiesFromPrompt.js
+++ frontend/src/utils/getPropertiesFromPrompt.js
@@ -2,8 +2,8 @@ import getLLMResponse from './getLLMResponse';
 
 const CATEGORIES = ['Lakes', 'Parks', 'Dineouts', 'Temples', 'Viewpoints'];
 
-const getPropertyFromPrompt = prompt => {
-  const categories = getLLMResponse({
+const getPropertyFromPrompt = async prompt => {
+  const categories = await getLLMResponse({
     question: `I have a set of categories which are ${CATEGORIES.join(
       ', ',
     )}. Out of these 5 categories, what are the categories mentioned in the statement I have supplied? Give them to me in comma separated format.`,
@@ -12,7 +12,7 @@ const getPropertyFromPrompt = prompt => {
 
   console.log('categories', categories);
 
-  const timeConstraint = getLLMResponse({
+  const timeConstraint = await getLLMResponse({
     question:
       'What is the time constraint mentioned in the statement? How much time is the user willing to spend on their journey? Give me the time in minutes.',
     statement: prompt,
@@ -20,7 +20,7 @@ const getPropertyFromPrompt = prompt => {
 
   console.log('timeConstraint', timeConstraint);
 
-  const additionalConstraints = getLLMResponse({
+  const additionalConstraints = await getLLMResponse({
     question: `Apart from the categories (which are ${categories}) and time constraint (which is ${timeConstraint} minutes), what are the additional constraints mentioned in the statement?`,
     statement: prompt,
   });
@@ -28,8 +28,8 @@ const getPropertyFromPrompt = prompt => {
   console.log('additionalConstraints', additionalConstraints);
 
   return {
-    categories, // 
-    timeConstraint, //
+    categories,
+    timeConstraint,
     additionalConstraints,
   };
 };
