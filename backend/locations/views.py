41b4d157eb4c1fb8679687020033dcc6343fb84e
basichacker881 <mugdhasharma0327@gmail.com>
2023-11-04 18:44:08 +0530

    adds extract paths from source and destination

--- backend/locations/views.py
+++ backend/locations/views.py
@@ -1,3 +1,68 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from utils.getFeelGoodPath import getFeelGoodPaths
 
-# Create your views here.
+"""
+example json needed after GPT simplifies the string
+{
+    "source_lon": 77.681345 ,   decimal field upto length 30 and 20 decimal places
+    "source_lat":  13.112519,   decimal field upto length 30 and 20 decimal places
+    "destination":"Cubbon Park",  string field
+    "time": 15000,   minutes, integer field
+    "categories": "Lakes, Temples"     can be of 5 types - Lakes, Parks, Dineouts, Temples, Viewpoints all comma separated, string field
+    "extra_requirements": "I want to take my dog as well so add places only in which dogs are allowed"   string field
+}
+"""
+
+# TODO- complete function that simplifies the string using GPT into above json format
+
+
+def simplifyString(string):
+    response = {
+        "source_lon": 77.681345,
+        "source_lat":  13.112519,
+        "destination": "Cubbon Park",
+        "time": 2500,
+        "categories": "Lakes, Temples"
+    }
+    return response  # should return the above example json format
+
+# TODO- feed in all the paths in gpt4 that getFeelGoodPath returns and return the best 3
+
+
+def analyzeRoutes(paths, conditions=None):
+    pass
+
+
+class RouteView(APIView):
+    def post(self, request):
+
+        text = request.data['text']
+
+        data = simplifyString(text)
+        source_lat = data['source_lat']
+        source_lon = data['source_lon']
+        destination = data['destination']
+        time = data['time']
+        # convert comma separated string to list
+        categories = data['categories'].split(',')
+
+        # get all the paths in list format
+        paths = getFeelGoodPaths(source_lat, source_lon, destination, categories, time) #list of paths
+        print(paths)
+
+        # resultant_routes = analyzeRoutes(paths, data['extra_requirements'])
+        resultant_routes = []
+
+        return Response(resultant_routes, status=status.HTTP_200_OK)
+
+
+
+#paths example output:
+"""
+[[1, 2144.7, [['source', 13.112519, 77.681345], ['Sankey Tank', 13.011181, 77.574593], ['Cubbon Park', 12.974244, 77.592195]]], [1, 2284.2, [['source', 13.112519, 77.681345], ['Nagavara lake', 13.045011, 77.609064], ['Cubbon Park', 12.974244, 77.592195]]], [1, 2289.2, [['source', 13.112519, 77.681345], ['Ramapura Kere', 13.047197, 77.685142], ['Cubbon Park', 12.974244, 77.592195]]], [1, 2327.1, [['source', 13.112519, 77.681345], ['Kempambudhi Kere', 12.958467, 77.56093], ['Cubbon Park', 12.974244, 77.592195]]], [1, 2395.6, [['source', 13.112519, 77.681345], ['Bagmane Lake', 12.979188, 77.655151], ['Cubbon Park', 12.974244, 77.592195]]], [0, 1603.1, [['source', 13.112519, 77.681345], ['Cubbon Park', 12.974244, 77.592195]]]]
+
+
+each path starts from source and ends on destination (here cubbon park). each stop is in format of [name, lat, lon]
+"""
--- backend/utils/getFeelGoodPath.py
+++ backend/utils/getFeelGoodPath.py
@@ -1,5 +1,9 @@
-import requests
 import heapq
+import requests
+from rest_framework import status
+from locations.models import Place
+from rest_framework.response import Response
+
 
 class Node:
     def __init__(self, name, lat, lon):
@@ -8,51 +12,95 @@ class Node:
         self.lon = lon
         self.edges = []
 
+
 class Edge:
     def __init__(self, to, timeReq, feel_good):
         self.to = to
         self.timeReq = timeReq
         self.feel_good = feel_good
 
-def getFeelGoodPath(source, destination, available_time):
+
+def get_lat_lon(place_name):
+    nominatim_endpoint = "https://nominatim.openstreetmap.org/search"
+    params = {
+        'q': place_name,
+        'format': 'json',
+    }
+    response = requests.get(nominatim_endpoint, params=params)
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+
+        if len(data) > 0:
+            # Extract the latitude and longitude from the first result
+            lat = data[0]['lat']
+            lon = data[0]['lon']
+            return Response({'lat': lat, 'lon': lon}, status=status.HTTP_200_OK)
+        else:
+            return Response({'message': 'No coordiantes found for this destiantion'}, status=status.HTTP_404_NOT_FOUND)
+    else:
+        return Response({'message': 'Unable to access nominatim'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+def algoGetFeelGoodPath(source, destination, available_time):
     queue = [(0, 0, source, [])]
     visited = set()
+    top_paths = []  # list to store the top 3 paths
 
     while queue:
         (feel_good_count, time_spent, node, path) = heapq.heappop(queue)
 
+        path = path + [node]
+
+        if node == destination and time_spent <= available_time:
+            # add the path to the list
+            top_paths.append((feel_good_count, time_spent, path))
+            top_paths = sorted(top_paths, key=lambda x: x[0], reverse=True)
+            continue
+
         if node not in visited:
             visited.add(node)
-            path = path + [node]
-
-            if node == destination:
-                if time_spent <= available_time:
-                    return (-feel_good_count, time_spent, path)
-            
-            print('node: ', node.name)
             for edge in node.edges:
                 if edge.to not in visited:
-                    print('edge.to: ', edge.to.name)
-                    total_feel_good = feel_good_count - edge.feel_good
+                    total_feel_good = feel_good_count + edge.feel_good
                     total_time = time_spent + edge.timeReq
-                    print('total_time: ', total_time)
-                    print('total_feel_good: ', -total_feel_good)
                     if total_time <= available_time:
-                        heapq.heappush(queue, (total_feel_good, total_time, edge.to, path))
-                        
-            print('\n')
+                        heapq.heappush(
+                            queue, (total_feel_good, total_time, edge.to, path))
 
-    return (0, 0, [])
+    # return the paths in the list
+    return top_paths
 
-__all__ = ['Node', 'Edge', 'Graph', 'getFeelGoodPath']
 
-if __name__ == "__main__":
-    # step 1: get nodes array from database
-    source_name = "Hulimavu Lake"
-    destination_name = "Chikkabelanduru lake"
-    nodes = [Node("Hulimavu Lake", 12.8700, 77.6042), Node("Gotigere Tank", 12.85335, 77.59037), Node("something", 12.87226, 77.65923), Node("Chikkabelanduru lake", 12.91226, 77.71923)]
-    
-    # step 2: call the api to get the time taken to travel between each node
+def getFeelGoodPaths(source_lat, source_lon, destination, categories, time):
+
+    # step 1: get lat lon coorindates of destination from the text using nominatim
+    response = get_lat_lon(destination)
+
+    if response.status_code == 200:
+        destination_lat = response.data['lat']
+        destination_lon = response.data['lon']
+    else:
+        return Response({'message': response.data['message']}, status=response.status_code)
+
+    # step 2: get nodes array from database
+    # feel_good_places = Place.objects.filter(category__in=categories)
+    feel_good_places = Place.objects.filter(category="Lakes")
+
+    nodes = list()
+
+    # append src and destiantion to nodes, src in start and destn at end
+    nodes.append(Node("source", source_lat, source_lon))
+    nodes.append(Node(destination, destination_lat, destination_lon))
+
+    count = 0
+
+    for node in feel_good_places:
+        count += 1
+        if count == 20:
+            break
+        nodes.append(Node(node.name, node.latitude, node.longitude))
+
     API_URL = 'http://router.project-osrm.org/table/v1/driving/'
     for node in nodes:
         API_URL += str(node.lon) + ',' + str(node.lat) + ';'
@@ -60,23 +108,20 @@ if __name__ == "__main__":
     API_URL += '?annotations=duration'
     response = requests.get(API_URL)
     durations = response.json()['durations']
-    
-    print('durations: ', durations)
-    
-    # step 3: create the graph
+
+    # # step 3: create the graph
     for i in range(len(nodes)):
         for j in range(len(nodes)):
             if i == j:
                 continue
-            
+
             timeReq = durations[i][j]
-            feelGood = 1 if (nodes[j].name != source_name and nodes[j].name != destination_name) else 0
+            feelGood = 1 if (
+                nodes[j].name != "source" and nodes[j].name != destination) else 0
             nodes[i].edges.append(Edge(nodes[j], timeReq, feelGood))
 
-    # step 4: call the getFeelGoodPath function
-    (feel_good_count, time_spent, path) = getFeelGoodPath(nodes[0], nodes[3], 2500)
-    print('time spent: ', time_spent)
-    print('feel good count: ', feel_good_count)
-    for node in path:
-        print(node.name)
+    # src, destn, available_time
+    ans = algoGetFeelGoodPath(nodes[0], nodes[1], time)
+    converted_list = [[t[0], round(float(t[1]), 2), [[node.name, round(float(node.lat), 6), round(float(node.lon), 6)] for node in t[2]]] for t in ans]
+    return converted_list
     
