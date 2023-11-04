c988d61f56a9b38f5818b091cb5899a9462c92a5
Madhur Gupta <madhurgupta107@gmail.com>
2023-11-04 23:02:09 +0530

    Add getLLMResponse method

---
+++ frontend/src/utils/getLLMResponse.js
@@ -0,0 +1,36 @@
+import {OpenAI} from 'langchain/llms/openai';
+
+import {OPENAI_API_KEY} from 'react-native-dotenv';
+
+const model = new OpenAI({
+  modelName: 'gpt-3.5-turbo-16k',
+  openAIApiKey: OPENAI_API_KEY,
+});
+
+const getLLMResponse = async ({question = '', statement = ''}) => {
+  if (!question) return null;
+
+  const template = `
+      Model Name: (Bot Created by Biomi)
+  
+      Description: Your goal is to answer the questions in most appropriate ways and as precise as possible
+      
+      Instruction: You will be given a statement and a question and you'll have to answer the question with one word or as directed and the answer should only be derived from that statement only and in case if the answer is not avalaible in the statement please return "-" as answer.
+      ----------------------------------------------------------------------------------------
+      Statement:-
+      ${statement}
+  
+      ----------------------------------------------------------------------------------------
+      Question: ${question}
+      `;
+
+  try {
+    const res1 = await model.call(template);
+    return res1;
+  } catch (error) {
+    console.log({error});
+    console.log(error.response);
+  }
+};
+
+export default getLLMResponse;
